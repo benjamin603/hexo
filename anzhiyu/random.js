fdd8a76ec47b21f27ce5c80d32c1ab98b0e6086a
benjamin603 <benjamin.jowe@gmail.com>
2023-06-01 08:18:51 +0800

    Site updated: 2023-06-01 08:18:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/adad.html","posts/4119.html","posts/ea74.html","posts/52303.html","posts/57876.html","posts/28135.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/adad.html","posts/4119.html","posts/52303.html","posts/ea74.html","posts/57876.html","posts/28135.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
